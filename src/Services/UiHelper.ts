10fd0e79af766be32f1d2b5bb3a0058080ceb728
Justin Unterreiner <justin.unterreiner@gmail.com>
2015-09-08 20:24:50 -0700

    Reworked plugin accessor properties logic to be more straightforward

--- src/Services/UiHelper.ts
+++ src/Services/UiHelper.ts
@@ -57,10 +57,37 @@
         //#region Plug-in Accessors
 
         /**
+         * Exposes an API for working with native dialogs and notifications.
+         * 
+         * NOTE: Standard alert, confirm, and prompt dialogs should always use the corresponding
+         * methods from the UiHelper services rather than the notification plugin directly; the
+         * UiHelper methods normalize behavior for Ripple as well as standard buttons and titles.
+         */
+        get notification(): Notification {
+            if (this.Utilities.isRipple
+                || this.Utilities.isWindowsIoT) {
+                return this.MockPlatformApis.getNotificationPlugin();
+            }
+            else if (navigator.notification) {
+                return navigator.notification;
+            }
+            else {
+                return this.MockPlatformApis.getNotificationPlugin();
+            }
+        }
+
+        /**
          * Exposes an API for showing toast messages.
          */
         get toast(): ICordovaToastPlugin {
-            if (!this.Utilities.isRipple && !this.Utilities.isWindows && !this.Utilities.isWindows8 && window.plugins && window.plugins.toast) {
+
+            if (this.Utilities.isRipple
+                || this.Utilities.isWindows
+                || this.Utilities.isWindows8
+                || this.Utilities.isWindowsIoT) {
+                return this.MockPlatformApis.getToastPlugin();
+            }
+            else if (window.plugins && window.plugins.toast) {
                 return window.plugins.toast;
             }
             else {
@@ -72,7 +99,14 @@
          * Exposes an API for working with progress indicators.
          */
         get progressIndicator(): ICordovaProgressIndicator {
-            if (!this.Utilities.isRipple && !this.Utilities.isWindows && window.ProgressIndicator && !this.Utilities.isAndroid) {
+
+            if (this.Utilities.isRipple
+                || this.Utilities.isWindows
+                || this.Utilities.isWindows8
+                || this.Utilities.isWindowsIoT) {
+                return this.MockPlatformApis.getProgressIndicatorPlugin();
+            }
+            else if (window.ProgressIndicator) {
                 return window.ProgressIndicator;
             }
             else {
@@ -84,14 +118,20 @@
          * Exposes an API for working with the operating system's clipboard.
          */
         get clipboard(): ICordovaClipboardPlugin {
-            if (this.Utilities.isWindows) {
-                return this.MockPlatformApis.getClipboardPluginForWindows();
+
+            if (this.Utilities.isRipple) {
+                return this.MockPlatformApis.getClipboardPlugin();
             }
-            else if (!this.Utilities.isRipple && typeof(cordova) !== "undefined" && cordova.plugins && cordova.plugins.clipboard) {
-                return cordova.plugins.clipboard;
+            else if (this.Utilities.isWindows
+                || this.Utilities.isWindows8
+                || this.Utilities.isWindowsIoT) {
+                return this.MockPlatformApis.getClipboardPluginForWindows();
             }
             else if (this.Utilities.isChromeExtension) {
                 return this.MockPlatformApis.getClipboardPluginForChromeExtension();
+            }
+            else if (cordova.plugins && cordova.plugins.clipboard) {
+                return cordova.plugins.clipboard;
             }
             else {
                 return this.MockPlatformApis.getClipboardPlugin();
@@ -102,7 +142,11 @@
          * Exposes an API for manipulating the device's native status bar.
          */
         get statusBar(): StatusBar {
-            if (!this.Utilities.isRipple && window.StatusBar) {
+
+            if (this.Utilities.isRipple) {
+                return this.MockPlatformApis.getStatusBarPlugin();
+            }
+            else if (window.StatusBar) {
                 return window.StatusBar;
             }
             else {
@@ -114,7 +158,11 @@
          * Exposes an API for adjusting keyboard behavior.
          */
         get keyboard(): Ionic.Keyboard {
-            if (!this.Utilities.isRipple && typeof(cordova) !== "undefined" && cordova.plugins && cordova.plugins.Keyboard) {
+
+            if (this.Utilities.isRipple) {
+                return this.MockPlatformApis.getKeyboardPlugin();;
+            }
+            else if (typeof(cordova) !== "undefined" && cordova.plugins && cordova.plugins.Keyboard) {
                 return cordova.plugins.Keyboard;
             }
             else {
@@ -126,7 +174,11 @@
          * Exposes an API for logging exception information to the Crashlytics backend service.
          */
         get crashlytics(): ICordovaCrashlyticsPlugin {
-            if (!this.Utilities.isRipple && typeof(navigator) !== "undefined" && navigator.crashlytics) {
+
+            if (this.Utilities.isRipple) {
+                return this.MockPlatformApis.getCrashlyticsPlugin();
+            }
+            else if (typeof(navigator) !== "undefined" && navigator.crashlytics) {
                 return navigator.crashlytics;
             }
             else {
