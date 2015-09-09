c3a88e71c1e192cabc779f4d8bfa0a02815fc57b
Justin Unterreiner <justin.unterreiner@gmail.com>
2015-09-08 20:23:35 -0700

    Reworked platform detection utilities to use the local device property

--- src/Controllers/Settings/DeveloperController.ts
+++ src/Controllers/Settings/DeveloperController.ts
@@ -46,7 +46,7 @@
 
             this.viewModel.logToLocalStorage = this.Logger.getLogToLocalStorage();
 
-            this.viewModel.devicePlatform = this.Utilities.platform;
+            this.viewModel.devicePlatform = this.Utilities.device.platform;
             this.viewModel.deviceModel = this.Utilities.device.model;
             this.viewModel.deviceOsVersion = this.Utilities.device.version;
             this.viewModel.deviceUuid = this.Utilities.device.uuid;
